42b4f7d76cf1f00828f8ebf4137a834733e57824
michaeltconnelly <mconnelly@rsmas.miami.edu>
2019-12-03 09:36:34 -0500

    changes to coral STAR alignment wrapper

--- bash/STARalignwrapper_Pdam_v2.sh
+++ bash/STARalignwrapper_Pdam_v2.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#./bash/STARalignwrapper_Pdam_v2.sh
+#purpose: align trimmed RNAseq reads against the Pocillopora damicornis genome using STAR on the Pegasus bigmem queue
+#version 2: two-pass alignment for improved splice junction detection accuracy, downstream SNP calling and phylotranscriptomics, output non-aligning reads for Symbiodinaceae analysis
+#To start this job from the EAPSI_Pocillopora_LPS directory, use:
+#bsub -P transcriptomics < /scratch/projects/transcriptomics/mikeconnelly/projects/EAPSI_Pocillopora_LPS/bash/STARalignwrapper_Pdam_v2.sh
+
 #BSUB -J starwrap_pdam
 #BSUB -q general
 #BSUB -P transcriptomics
@@ -7,16 +13,11 @@
 #BSUB -n 8
 #BSUB -u m.connelly1@umiami.edu
 
-#~/scripts/EAPSI.HW-WT-master/STARalignwrapper_Pdam_v2.sh
-#/scratch/projects/transcriptomics/mikeconnelly/scripts/EAPSI.HW-WT-master/STARalignwrapper_Pdam_v2.sh
-#purpose: align trimmed RNAseq reads against the Pocillopora damicornis genome using STAR on the Pegasus bigmem queue
-#version 2: two-pass alignment for improved splice junction detection accuracy, downstream SNP calling and phylotranscriptomics, output non-aligning reads for Symbiodinaceae analysis
-
 #specify variable containing sequence file prefixes and directory paths
 mcs="/scratch/projects/transcriptomics/mikeconnelly"
 coldir="/scratch/projects/transcriptomics/mikeconnelly/sequences/EAPSI/houwanwanglitung"
 exp="alltreatments"
-EAPSIsamples="Wt1-1a Wt1-1b Wt1-1c Wt1-2a Wt1-2b Wt1-3a Wt1-3b Wt1-3c Wt1-4a Wt1-4b Wt1-4c Wt1-5a Wt1-5b Wt1-5c Wt1-6a Wt1-6b Wt1-6c Wt2-1a Wt2-1b Wt2-1c Wt2-2b Wt2-2c Wt2-3a Wt2-3b Wt2-3c Wt2-4a Wt2-4b Wt2-4c Wt2-5a Wt2-5b Wt2-5c Wt2-6a Wt2-6b Wt2-6c Hw1-1a Hw1-1b Hw1-1c Hw1-2a Hw1-2b Hw1-2c Hw1-3a Hw1-3b Hw1-3c Hw1-4a Hw1-4b Hw1-4c Hw1-5a Hw1-5b Hw1-5c Hw1-6a Hw1-6b Hw1-6c Hw2-1a Hw2-1b Hw2-1c Hw2-2a Hw2-2b Hw2-2c Hw2-3a Hw2-3b Hw2-3c Hw2-4a Hw2-4b Hw2-4c Hw2-5a Hw2-5b Hw2-5c Hw2-6b Hw2-6c"
+EAPSIsamples="Wt1-3a Wt1-3b Wt1-3c Wt1-6a Wt1-6b Wt1-6c Wt2-3a Wt2-3b Wt2-3c Wt2-6a Wt2-6b Wt2-6c Hw1-3a Hw1-3b Hw1-3c Hw1-6a Hw1-6b Hw1-6c Hw2-3a Hw2-3b Hw2-3c Hw2-6b Hw2-6c"
 
 #lets me know which files are being processed
 echo "These are the reads to be aligned to the Pocillopora reference genome: $EAPSIsamples"
