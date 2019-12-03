6f0205b879c95fb5be8c4c1f683cfdb33cd4aea0
michaeltconnelly <mconnelly@rsmas.miami.edu>
2019-12-03 09:50:45 -0500

    changes to featureCounts

--- bash/STARfC_Pdam.sh
+++ bash/STARfC_Pdam.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#~/scripts/EAPSI.HW-WT-master/STARfC_Pdam.sh
-#/scratch/projects/transcriptomics/mikeconnelly/scripts/EAPSI.HW-WT-master/STARfC_Pdam.sh
+#./bash/STARfC_Pdam.sh
 #purpose: quantify Pocillopora damicornis aligned RNAseq transcript abundances using featureCounts program to create a counts table on Pegasus
+#To start this job from the EAPSI_Pocillopora_LPS directory, use:
+#bsub -P transcriptomics < /scratch/projects/transcriptomics/mikeconnelly/projects/EAPSI_Pocillopora_LPS/bash/STARfC_Pdam.sh
 
 #BSUB -J starfC_pdam
 #BSUB -q general
@@ -13,11 +14,12 @@
 #BSUB -u mconnelly@rsmas.miami.edu
 #BSUB -N
 
-#specify variable containing sequence file prefixes, experiment design and directory paths
+#specify variable containing sequence file prefixes and directory paths
 mcs="/scratch/projects/transcriptomics/mikeconnelly"
 coldir="/scratch/projects/transcriptomics/mikeconnelly/sequences/EAPSI/houwanwanglitung"
 exp="alltreatments"
-EAPSIsamples="Wt1-1a Wt1-1b Wt1-1c Wt1-2a Wt1-2b Wt1-3a Wt1-3b Wt1-3c Wt1-4a Wt1-4b Wt1-4c Wt1-5a Wt1-5b Wt1-5c Wt1-6a Wt1-6b Wt1-6c Wt2-1a Wt2-1b Wt2-1c Wt2-2b Wt2-2c Wt2-3a Wt2-3b Wt2-3c Wt2-4a Wt2-4b Wt2-4c Wt2-5a Wt2-5b Wt2-5c Wt2-6a Wt2-6b Wt2-6c Hw1-1a Hw1-1b Hw1-1c Hw1-2a Hw1-2b Hw1-2c Hw1-3a Hw1-3b Hw1-3c Hw1-4a Hw1-4b Hw1-4c Hw1-5a Hw1-5b Hw1-5c Hw1-6a Hw1-6b Hw1-6c Hw2-1a Hw2-1b Hw2-1c Hw2-2a Hw2-2b Hw2-2c Hw2-3a Hw2-3b Hw2-3c Hw2-4a Hw2-4b Hw2-4c Hw2-5a Hw2-5b Hw2-5c Hw2-6b Hw2-6c"
+EAPSIsamples="Wt1-3a Wt1-3b Wt1-3c Wt1-6a Wt1-6b Wt1-6c Wt2-3a Wt2-3b Wt2-3c Wt2-6a Wt2-6b Wt2-6c Hw1-3a Hw1-3b Hw1-3c Hw1-6a Hw1-6b Hw1-6c Hw2-3a Hw2-3b Hw2-3c Hw2-6b Hw2-6c"
+
 echo "These are the .bam files to be quantified using featureCounts"
 echo $EAPSIsamples
 ${mcs}/programs/subread-1.6.0-Linux-x86_64/bin/featureCounts -t gene \
