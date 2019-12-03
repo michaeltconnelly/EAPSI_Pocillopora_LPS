2dbd3b2a0816a6014b05e3dbe56b17014ea162f0
michaeltconnelly <mconnelly@rsmas.miami.edu>
2019-12-03 11:16:26 -0500

    began assembling the direct R script

--- R/EAPSI_Pocillopora_LPS.r
+++ R/EAPSI_Pocillopora_LPS.r
@@ -1,1 +1,13 @@
 #EAPSI_Pocillopora_LPS.r
+#author: "Mike Connelly"
+#date: "12/03/2019"
+
+library("tidyverse")
+library("DESeq2")
+library("ggpubr")
+library("apeglm")
+
+#Import sample data
+stable_samples <- read.table("./data/EAPSIsamples_stable.txt", header = TRUE)
+#Import counts data
+countdata <- read.delim("LPS_Pdam.counts", row.names = 1, skip = 1)
