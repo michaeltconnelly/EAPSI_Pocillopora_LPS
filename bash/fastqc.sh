619b1955df66a21e7dbd016931b59457b8e03447
michaeltconnelly <mconnelly@rsmas.miami.edu>
2019-12-03 10:54:51 -0500

    change exp to LPS from alltreatments

--- bash/fastqc.sh
+++ bash/fastqc.sh
@@ -16,7 +16,7 @@
 #specify variable containing sequence file prefixes and directory paths
 mcs="/scratch/projects/transcriptomics/mikeconnelly"
 coldir="/scratch/projects/transcriptomics/mikeconnelly/sequences/EAPSI/houwanwanglitung"
-exp="alltreatments"
+exp="LPS"
 EAPSIsamples="Wt1-3a Wt1-3b Wt1-3c Wt1-6a Wt1-6b Wt1-6c Wt2-3a Wt2-3b Wt2-3c Wt2-6a Wt2-6b Wt2-6c Hw1-3a Hw1-3b Hw1-3c Hw1-6a Hw1-6b Hw1-6c Hw2-3a Hw2-3b Hw2-3c Hw2-6b Hw2-6c"
 
 module load java/1.8.0_60
